c1755cef46457baf1975b466254fb61dd06d19f4
MoonLGH <moonlmc1212@gmail.com>
2021-11-26 00:19:00 +0700

    im ded inside 4

---
+++ commands/translate.ts
@@ -0,0 +1,155 @@
+import { Client, Message } from "@open-wa/wa-automate";
+import translate from '@vitalets/google-translate-api';
+
+var langs = {
+    'auto': 'Automatic',
+    'af': 'Afrikaans',
+    'sq': 'Albanian',
+    'am': 'Amharic',
+    'ar': 'Arabic',
+    'hy': 'Armenian',
+    'az': 'Azerbaijani',
+    'eu': 'Basque',
+    'be': 'Belarusian',
+    'bn': 'Bengali',
+    'bs': 'Bosnian',
+    'bg': 'Bulgarian',
+    'ca': 'Catalan',
+    'ceb': 'Cebuano',
+    'ny': 'Chichewa',
+    'zh-CN': 'Chinese (Simplified)',
+    'zh-TW': 'Chinese (Traditional)',
+    'co': 'Corsican',
+    'hr': 'Croatian',
+    'cs': 'Czech',
+    'da': 'Danish',
+    'nl': 'Dutch',
+    'en': 'English',
+    'eo': 'Esperanto',
+    'et': 'Estonian',
+    'tl': 'Filipino',
+    'fi': 'Finnish',
+    'fr': 'French',
+    'fy': 'Frisian',
+    'gl': 'Galician',
+    'ka': 'Georgian',
+    'de': 'German',
+    'el': 'Greek',
+    'gu': 'Gujarati',
+    'ht': 'Haitian Creole',
+    'ha': 'Hausa',
+    'haw': 'Hawaiian',
+    'he': 'Hebrew',
+    'iw': 'Hebrew',
+    'hi': 'Hindi',
+    'hmn': 'Hmong',
+    'hu': 'Hungarian',
+    'is': 'Icelandic',
+    'ig': 'Igbo',
+    'id': 'Indonesian',
+    'ga': 'Irish',
+    'it': 'Italian',
+    'ja': 'Japanese',
+    'jw': 'Javanese',
+    'kn': 'Kannada',
+    'kk': 'Kazakh',
+    'km': 'Khmer',
+    'ko': 'Korean',
+    'ku': 'Kurdish (Kurmanji)',
+    'ky': 'Kyrgyz',
+    'lo': 'Lao',
+    'la': 'Latin',
+    'lv': 'Latvian',
+    'lt': 'Lithuanian',
+    'lb': 'Luxembourgish',
+    'mk': 'Macedonian',
+    'mg': 'Malagasy',
+    'ms': 'Malay',
+    'ml': 'Malayalam',
+    'mt': 'Maltese',
+    'mi': 'Maori',
+    'mr': 'Marathi',
+    'mn': 'Mongolian',
+    'my': 'Myanmar (Burmese)',
+    'ne': 'Nepali',
+    'no': 'Norwegian',
+    'ps': 'Pashto',
+    'fa': 'Persian',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'pa': 'Punjabi',
+    'ro': 'Romanian',
+    'ru': 'Russian',
+    'sm': 'Samoan',
+    'gd': 'Scots Gaelic',
+    'sr': 'Serbian',
+    'st': 'Sesotho',
+    'sn': 'Shona',
+    'sd': 'Sindhi',
+    'si': 'Sinhala',
+    'sk': 'Slovak',
+    'sl': 'Slovenian',
+    'so': 'Somali',
+    'es': 'Spanish',
+    'su': 'Sundanese',
+    'sw': 'Swahili',
+    'sv': 'Swedish',
+    'tg': 'Tajik',
+    'ta': 'Tamil',
+    'te': 'Telugu',
+    'th': 'Thai',
+    'tr': 'Turkish',
+    'uk': 'Ukrainian',
+    'ur': 'Urdu',
+    'uz': 'Uzbek',
+    'vi': 'Vietnamese',
+    'cy': 'Welsh',
+    'xh': 'Xhosa',
+    'yi': 'Yiddish',
+    'yo': 'Yoruba',
+    'zu': 'Zulu'
+};
+
+export = {
+    name: "stickergif",
+    description: "Sticker GIF",
+    execute: async (message: Message, args: string[], client: Client) => {
+        if(args.length < 1) return client.reply(message.chatId, "please do \n translate <TO Optional> FROM Text",message.id)
+        let from,to,translates
+        from = "auto"
+        to = args.shift()
+
+        for(let lang in langs){
+            if(lang.toLowerCase() == args[0].toLowerCase()){
+                from = args.shift()
+            }
+        }
+
+        translates = args.join(" ")
+    
+        if(!to || from) return client.reply(message.chatId, "please do \n translate <TO Optional> FROM Text",message.id)
+        if(!isSupported(to!) || !isSupported(from!)){
+            return client.reply(message.chatId, "please do \n translate <TO Optional> FROM Text",message.id)
+        }
+        let late = await translate(translates, {
+            to: to,
+            from: from
+        })
+
+        client.reply(message.chatId,`FROM : ${late.from.language.iso} \n TO : ${to}`,message.id)
+        client.reply(message.chatId,`${late.text}`,message.id)
+    }
+}
+
+function isSupported(desiredLang:string) {
+    return Boolean(getCode(desiredLang));
+}
+
+function getCode(desiredLang:string) {
+    for(let lang in langs){
+        if(lang.toLowerCase() == desiredLang.toLowerCase()){
+            return lang;
+        }
+    }
+    return null;
+}
