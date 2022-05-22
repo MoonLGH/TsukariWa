0d76f25f11d5fdf565526b70479d2562ed0bc681
MoonLGH <MoonLMC1212@gmail.com>
2022-05-22 18:17:13 +0000

    using parse

--- src/commands/Misc/isSleep.ts
+++ src/commands/Misc/isSleep.ts
@@ -6,7 +6,7 @@ export = {
     execute: async (Message: Message, args: string[],client:Client) => {
         if(Message.fromMe){
             let Data = await import("../../util/GlobalVar")
-            Data.isSleep = Boolean(args[0])
+            Data.isSleep = JSON.parse(args[0])
             client.reply(Message.chatId, `Understand able its ${args[0]} now`,Message.id)
         }else{
             client.reply(Message.chatId, "Only the owner can use this command",Message.id)
